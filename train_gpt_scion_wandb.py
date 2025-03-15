554e555c20bbe7b8550443adb32ca59d916fcfba
Artem <142977489+artem-riabinin@users.noreply.github.com>
2025-03-15 18:12:31 +0300

    upd-1

--- train_gpt_scion_wandb.py
+++ train_gpt_scion_wandb.py
@@ -451,8 +451,8 @@ def get_lr(it):
 schedulers = [torch.optim.lr_scheduler.LambdaLR(opt, get_lr) for opt in optimizers]
 
 # wandb logging
-config_keys = [k for k,v in globals().items() if not k.startswith('_') and isinstance(v, (int, float, bool, str))]
-configuration = {k: globals()[k] for k in config_keys}
+config_keys = [k for k, v in vars(args).items() if not k.startswith('_') and isinstance(v, (int, float, bool, str))]
+configuration = {k: vars(args)[k] for k in config_keys}
 if wandb_log and master_process:
     import wandb
     wandb.init(project=wandb_project, name=wandb_run_name, config=configuration)

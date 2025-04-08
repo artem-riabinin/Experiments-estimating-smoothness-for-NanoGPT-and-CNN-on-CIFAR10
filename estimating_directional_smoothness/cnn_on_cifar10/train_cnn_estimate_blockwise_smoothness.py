778a99697810e1397956df12afc6b8a8a579f07f
Artem <artem.riabinin@kaust.edu.sa>
2025-04-08 18:05:10 +0300

    no message

--- estimating_directional_smoothness/cnn_on_cifar10/train_cnn_estimate_blockwise_smoothness.py
+++ estimating_directional_smoothness/cnn_on_cifar10/train_cnn_estimate_blockwise_smoothness.py
@@ -40,6 +40,41 @@ wandb_run_name = 'cnn_unScion_estimate_blockwise_smoothness'
 #               Scion optimizer              #
 #############################################
 
+@torch.compile
+def zeropower_via_newtonschulz5(G, steps=5):
+    """
+    From: https://github.com/KellerJordan/modded-nanogpt/blob/master/records/101724_DistributedMuon/22d24867-eb5a-4fcc-ae2c-263d0277dfd1.txt
+    Newton-Schulz iteration to compute the zeroth power / orthogonalization of G. We opt to use a
+    quintic iteration whose coefficients are selected to maximize the slope at zero. For the purpose
+    of minimizing steps, it turns out to be empirically effective to keep increasing the slope at
+    zero even beyond the point where the iteration no longer converges all the way to one everywhere
+    on the interval. This iteration therefore does not produce UV^T but rather something like US'V^T
+    where S' is diagonal with S_{ii}' ~ Uniform(0.5, 1.5), which turns out not to hurt model
+    performance at all relative to UV^T, where USV^T = G is the SVD.
+    """
+    assert len(G.shape) == 2
+    a, b, c = (3.4445, -4.7750,  2.0315)
+    X = G.bfloat16()
+    if G.size(0) > G.size(1):
+        X = X.T
+
+    # Ensure spectral norm is at most 1
+    X = X / (X.norm() + 1e-7)
+    # Perform the NS iterations
+    for _ in range(steps):
+        A = X @ X.T
+        B = b * A + c * A @ A # adapted from suggestion by @jxbz, @leloykun, and @YouJiacheng
+        X = a * X + B @ X
+    
+    if G.size(0) > G.size(1):
+        X = X.T
+    return X
+
+
+def zeroth_power_via_svd(G):
+   U, S, V = G.svd()
+   return U @ V.T
+
 class Norm(object):
     def lmo(self, g):
         raise NotImplementedError

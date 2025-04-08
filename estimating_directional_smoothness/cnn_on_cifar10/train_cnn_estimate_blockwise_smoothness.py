23490ea2928729e7342efda90052b763d58cf019
Artem <artem.riabinin@kaust.edu.sa>
2025-04-09 00:09:14 +0300

    no message

--- estimating_directional_smoothness/cnn_on_cifar10/train_cnn_estimate_blockwise_smoothness.py
+++ estimating_directional_smoothness/cnn_on_cifar10/train_cnn_estimate_blockwise_smoothness.py
@@ -705,7 +705,7 @@ if __name__ == "__main__":
 
     # We re-use the compiled model between runs to save the non-data-dependent compilation time
     model = CifarNet().cuda().to(memory_format=torch.channels_last)
-    model.compile(mode="max-autotune")
+    model.compile(mode="default")
 
     print_columns(logging_columns_list, is_head=True)
     main("warmup", model)

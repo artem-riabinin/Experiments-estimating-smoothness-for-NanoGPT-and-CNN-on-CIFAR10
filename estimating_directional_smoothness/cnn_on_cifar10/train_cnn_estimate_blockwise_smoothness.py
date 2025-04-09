eb4fc3d3be38fcb7df22071cb8b31b0f1e9a4950
Artem <artem.riabinin@kaust.edu.sa>
2025-04-09 17:19:53 +0300

    no message

--- estimating_directional_smoothness/cnn_on_cifar10/train_cnn_estimate_blockwise_smoothness.py
+++ estimating_directional_smoothness/cnn_on_cifar10/train_cnn_estimate_blockwise_smoothness.py
@@ -366,7 +366,7 @@ class CifarLoader:
         data = torch.load(data_path, map_location=torch.device("cuda"))
         self.images, self.labels, self.classes = data["images"], data["labels"], data["classes"]
         # It's faster to load+process uint8 data than to load preprocessed fp16 data
-        self.images = (self.images.half() / 255).permute(0, 3, 1, 2).to(memory_format=torch.channels_last)
+        self.images = (self.images.float() / 255).permute(0, 3, 1, 2).to(memory_format=torch.channels_last)
 
         self.normalize = T.Normalize(CIFAR_MEAN, CIFAR_STD)
         self.proc_images = {} # Saved results of image processing to be done on the first epoch

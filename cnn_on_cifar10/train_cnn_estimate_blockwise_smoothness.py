b2c673c63d74b9302769131621e18e91df10d846
Artem <artem.riabinin@kaust.edu.sa>
2025-04-10 02:14:44 +0300

    no message

--- cnn_on_cifar10/train_cnn_estimate_blockwise_smoothness.py
+++ cnn_on_cifar10/train_cnn_estimate_blockwise_smoothness.py
@@ -599,7 +599,7 @@ def main(model):
         'norm_kwargs': {'normalized': True},
         'scale': radius*100.0,
     }]
-    optimizer1 = Scion(optim_groups, lr=2**-2, momentum=0.5, unconstrained=True)
+    optimizer1 = Scion(optim_groups, lr=0.0625, momentum=0.5, unconstrained=True)
     optimizer1.init()
     optimizers = [optimizer1]
 

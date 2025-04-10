695a5665a92a80a097667b479e700be3788cb20a
Artem <artem.riabinin@kaust.edu.sa>
2025-04-10 12:50:40 +0300

    no message

--- cnn_on_cifar10/train_cnn_estimate_blockwise_smoothness.py
+++ cnn_on_cifar10/train_cnn_estimate_blockwise_smoothness.py
@@ -580,7 +580,7 @@ def main(model):
 
     test_loader = CifarLoader("cifar10", train=False, batch_size=2000)
     train_loader = CifarLoader("cifar10", train=True, batch_size=batch_size, aug=dict(flip=True, translate=2))
-    total_train_steps = ceil(16 * len(train_loader))
+    total_train_steps = ceil(8 * len(train_loader))
     
     # Create optimizers and schedulers
     filter_params = [p for p in model.parameters() if len(p.shape) == 4 and p.requires_grad]
@@ -589,7 +589,7 @@ def main(model):
     output_layer = [model.head.weight]
     radius = 1.0
     scale_factor = 100
-    learning_rate = 0.02
+    learning_rate = 0.05
     optim_groups = [{
         'params': remaining_parameters,
         'norm': 'Auto', # Picks layerwise norm based on the parameter shape
@@ -645,7 +645,7 @@ def main(model):
             for opt, sched in zip(optimizers, schedulers):
                 step_epoch = step / len(train_loader)
                 opt.step(step_epoch=step_epoch, iter=iter)
-                #sched.step()
+                sched.step()
             model.zero_grad(set_to_none=True)
             iter += 1
             step += 1
